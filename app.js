47b8712ddfcb3857e4ad77447d4cc6e6a6478d02
Tyler <tyhlewis@gmail.com>
2014-11-03 23:32:13 -0800

    (refactor) refactors to create tr and td elements with specific classes

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+// (function createBoard() {
+//   for(var i = 0; i < 4; i++) {
+//     var corner = document.createElement('div');
+//     corner.className = 'corner' + (i + 1);
+//     document.getElementsByClassName('gameboard')[0].appendChild(corner);
+//     for(var j = 0; j < 9; j++) {
+//       var space = document.createElement('div');
+//       space.className = 'section' + (i + 1) + ' space' + (j + 1);
+//       document.getElementsByClassName('gameboard')[0].appendChild(space);
+//     }
+//   }
+// })();
+
 (function createBoard() {
-  for(var i = 0; i < 4; i++) {
-    var corner = document.createElement('div');
-    corner.className = 'corner' + (i + 1);
-    document.getElementsByClassName('gameboard')[0].appendChild(corner);
-    for(var j = 0; j < 9; j++) {
+  for(var i = 0; i < 11; i++) {
+    var row = document.createElement('tr');
+    var rowClass = 'row' + (i + 1);
+    row.className = rowClass;
+    document.getElementsByClassName('gameboard')[0].appendChild(row);
+    for(var j = 0; j < 11; j++) {
+      var td = document.createElement('td');
       var space = document.createElement('div');
-      space.className = 'section' + (i + 1);
-      document.getElementsByClassName('gameboard')[0].appendChild(space);
+
+      if(i === 0 && j === 0) { var spaceClass = 'upperLeft'; }
+      else if(i === 0 && j === 10) { var spaceClass = 'upperRight'; }
+      else if(i === 10 && j === 0) { var spaceClass = 'lowerLeft'; }
+      else if(i === 10 && j === 10) { var spaceClass = 'lowerRight'; }
+      else if(i === 0) { var spaceClass = 'top'; }
+      else if(i === 10) { var spaceClass = 'bottom'; }
+      else if(j === 0) { var spaceClass = 'left'; }
+      else if(j === 10) { var spaceClass = 'right'; }
+      else { var spaceClass = 'nah'; }
+
+      space.className = spaceClass;
+      td.appendChild(space);
+      document.getElementsByClassName(rowClass)[0].appendChild(td);
     }
   }
 })();
